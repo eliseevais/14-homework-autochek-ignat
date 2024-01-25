5697d3e1c92d40646fb323603902b79b95f37151
Ирина Елисеева <irina.eliseeva1991@gmail.com>
2024-01-25 20:58:21 +0300

    sprint02 week03

--- src/s2-homeworks/hw03/HW3.tsx
+++ src/s2-homeworks/hw03/HW3.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react'
-import { v1 } from 'uuid'
+import React, {useState} from 'react'
+import {v1} from 'uuid'
 import s2 from '../../s1-main/App.module.css'
 import GreetingContainer from './GreetingContainer'
 
 /*
-* 1 - описать тип UserType
-* 2 - указать нужный тип в useState с users
-* 3 - дописать типы и логику функции pureAddUserCallback и проверить её тестами
+* 1 done - описать тип UserType
+* 2 done - указать нужный тип в useState с users
+* 3 done - дописать типы и логику функции pureAddUserCallback и проверить её
+*  тестами
 * 4 - в файле GreetingContainer.tsx дописать типизацию пропсов
 * 5 - в файле GreetingContainer.tsx указать нужные типы в useState с name и error
 * 6 - в файле GreetingContainer.tsx дописать тип и логику функции setNameCallback
@@ -19,36 +20,38 @@ import GreetingContainer from './GreetingContainer'
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+  _id: string // need to fix any
+  name: string // need to fix any
 }
 
-export const pureAddUserCallback = (name: any, setUsers: any, users: any) => { // need to fix any
-    const user = { // need to fix
-    }
-    setUsers([...users, user])
+export const pureAddUserCallback = (name: string, setUsers: (users: Array<UserType>) => void, users: Array<UserType>) => { // need to fix any
+  const user = { // need to fix
+    _id: v1(),
+    name: name
+  }
+  setUsers([...users, user])
 }
 
 const HW3 = () => {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+  const [users, setUsers] = useState<UserType[]>([]) // need to fix any
 
-    const addUserCallback = (name: any) => { // need to fix any
-        pureAddUserCallback(name, setUsers, users)
-    }
+  const addUserCallback = (name: string) => { // need to fix any
+    pureAddUserCallback(name, setUsers, users)
+  }
 
-    return (
-        <div id={'hw3'}>
-            <div className={s2.hwTitle}>Homework #3</div>
-            {/*для автоматической проверки дз (не менять)*/}
+  return (
+    <div id={'hw3'}>
+      <div className={s2.hwTitle}>Homework #3</div>
+      {/*для автоматической проверки дз (не менять)*/}
 
-            <div className={s2.hw}>
-                <GreetingContainer
-                    users={users}
-                    addUserCallback={addUserCallback}
-                />
-            </div>
-        </div>
-    )
+      <div className={s2.hw}>
+        <GreetingContainer
+          users={users}
+          addUserCallback={addUserCallback}
+        />
+      </div>
+    </div>
+  )
 }
 
 export default HW3
