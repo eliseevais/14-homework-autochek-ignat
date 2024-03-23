d044509e80de391fda79f9a1b4c58a644d2b7baa
Ирина Елисеева <irina.eliseeva1991@gmail.com>
2024-03-23 19:39:13 +0300

    sprint03 week04

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,29 @@
 import {UserType} from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: 'sort'; payload: 'up' | 'down' }
+  | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (state: Array<UserType>, action: ActionType): Array<UserType> => { // need to
+                                                                                                  // fix any
+  switch (action.type) {
+    case 'sort': { // by name
 
-            return state // need to fix
+      return [...state].sort((a: UserType, b: UserType) => {
+        if (a.name > b.name) {
+          return action.payload === 'up' ? 1 : - 1
         }
-        case 'check': {
-
-            return state // need to fix
+        if (a.name < b.name) {
+          return action.payload === 'up' ? -1 : 1
         }
-        default:
-            return state
+        return 0
+      })
     }
+    case 'check': {
+
+      return state.filter((user: UserType) => user.age > 17)
+    }
+    default:
+      return state
+  }
 }
