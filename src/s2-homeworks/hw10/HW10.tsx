bf33357949252d2355a36cae8eed3c6ddafad89c
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2024-04-24 22:42:58 +0300

    sprint04 week02 HW10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {AppStoreType} from './bll/store'
-import {loadingAC} from './bll/loadingReducer'
+import {initStateType, loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
@@ -14,35 +14,46 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
-    // useSelector, useDispatch // пишет студент
-    const isLoading = false
+  // useSelector, useDispatch // пишет студент
+  //const isLoading = false
 
-    const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
+  const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+  const dispatch = useDispatch();
 
-        // setTimeout
-    }
 
-    return (
-        <div id={'hw10'}>
-            <div className={s2.hwTitle}>Homework #10</div>
+  const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
+    // dispatch
+    //dispatch(loadingAC())
+    dispatch(loadingAC(true))
 
-            <div className={s2.hw}>
-                {isLoading ? (
-                    <div id={'hw10-loading'}>
-                        <Loader/>
-                    </div>
-                ) : (
-                    <SuperButton
-                        id={'hw10-button-start-loading'}
-                        onClick={setLoading}
-                    >
-                        Set loading...
-                    </SuperButton>
-                )}
-            </div>
-        </div>
-    )
+    console.log('isLoading')
+    // setTimeout
+    setTimeout(() => {
+      console.log('isLoading finished')
+      dispatch(loadingAC(false))
+    }, 1500)
+  }
+
+  return (
+    <div id={'hw10'}>
+      <div className={s2.hwTitle}>Homework #10</div>
+
+      <div className={s2.hw}>
+        {isLoading ? (
+          <div id={'hw10-loading'}>
+            <Loader/>
+          </div>
+        ) : (
+          <SuperButton
+            id={'hw10-button-start-loading'}
+            onClick={setLoading}
+          >
+            Set loading...
+          </SuperButton>
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default HW10
--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,5 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}>
+  <img src="https://cdn.dribbble.com/users/1787505/screenshots/7300251/media/a351d9e0236c03a539181b95faced9e0.gif" alt=""/>
+</div>
