40276811ca5f8f5df22e2b5a8bc648db32d68bee
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2024-05-22 23:24:44 +0300

    sprint04 week03: ranges

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import {Slider, SliderProps} from '@mui/material'
 
+// передать в пропсы min, max, value, onChange
 const SuperRange: React.FC<SliderProps> = (props) => {
-    return (
-        <Slider
-            sx={{ // стили для слайдера // пишет студент
-                
-            }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
-        />
-    )
+  return (
+    <Slider
+      sx={{ // стили для слайдера // пишет студент
+        color: '#00CC22',
+        width: '148px',
+        height: '4px',
+        marginRight: '14px'
+      }}
+      {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+    />
+  )
 }
 
 export default SuperRange
